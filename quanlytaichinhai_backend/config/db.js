3ee1b77b84dcead17cc467772bb24b71a87c9d85
Sang HT <huynhtsang2004@gmail.com>
2025-07-27 19:46:50 +0700

    fix db address

--- quanlytaichinhai_backend/config/db.js
+++ quanlytaichinhai_backend/config/db.js
@@ -3,12 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   connectionLimit: 10,
+// });
+
+//deploy 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT), // thêm dòng này
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 10,
 });
+
 
 export default db;
